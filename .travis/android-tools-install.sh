24c4ae058391a3c4c76fc23646c93c11249971af
Josh Wolfe <joshuafwolfe@gmail.com>
2018-07-05 18:40:12 -0400

    Build

--- .travis/android-tools-install.sh
+++ .travis/android-tools-install.sh
@@ -20,11 +20,11 @@ unzip -q android-ndk*.zip
 
 # Let it update itself and install some stuff
 cd android-sdk-linux/tools
-../../.travis/accept-licences.sh "./android update sdk --no-ui"
+../../.travis/accept-licences.sh "./android update sdk --no-ui && ./android update sdk --all --no-ui --filter $(seq -s, 27)"
 
 # Download every build-tools version that has ever existed
 # This will save you time! Thank me later for this
-./android update sdk --all --no-ui --filter $(seq -s, 27)
+# ./android update sdk --all --no-ui --filter $(seq -s, 27)
 
 # If you need additional packages for your app, check available packages with:
 # ./android list sdk --all
